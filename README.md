e30d5512c7f72291869688befd39cd54938552f0
Darrin-Lin <darrin.de88@gmail.com>
2023-12-06 16:21:23 +0800

    update hw0504 test case

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ diff my_output_file your_output_file
 
 ### HW05
 
-**12/6** Upload hw0503 test cases (is the **base** of the cap)
+**12/6** Upload hw0503 test cases (is the **base** of the cap) and hw0504 test cases and update test program.
 
 **12/3** Upload hw0504 and hw0505 test program and hw0505 test cases.
 
--- test05/04/hw0504.c
+++ test05/04/hw0504.c
@@ -3,7 +3,11 @@
 #include "./mytlv.h"
 
 int main()
-{  
+{
+    // uint8_t array[] = {0x01, 0x04, 0x00, 0x09, 0x05, 0x02, 0x07, 0x02,
+    //                    0x01, 0x00, 0x02, 0x07, 0x02, 0x00, 0x04, 0x05,
+    //                    0x0A, 0x00, 0x00, 0x09, 0x00, 0x00};
+    // int32_t size = sizeof(array)/sizeof(array[0]);
     int32_t size = 0;
     scanf("%d", &size);
     uint8_t array[size];
@@ -11,6 +15,6 @@ int main()
     {
         scanf("%hhd", &array[i]);
     }
-    run(array, size);
+    printf("return: %d\n",run(array, size));
     return 0;
 }
